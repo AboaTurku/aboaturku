d826a33fe3c11cd7dc1a59f9e8f669cc1b5eccb5
mattijv <matti.vapa@gmail.com>
2019-05-19 21:08:38 +0300

    Adds edit capability for the project page to the admin view.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,13 @@
 import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
+import ProjectPagePreview from './preview-templates/ProjectPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProfilePreview from './preview-templates/ProfilePreview'
 import PartnerPreview from './preview-templates/PartnerPreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('projects', ProjectPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('profile', ProfilePreview)
 CMS.registerPreviewTemplate('partner', PartnerPreview)
